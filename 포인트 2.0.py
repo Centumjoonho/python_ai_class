994c19f7139813409f1191fd74696a71495a3078
Centumjoonho <ghwnsgkgkgk@gmail.com>
2023-02-27 17:28:58 +0900

    2023-02-27 point

--- 포인트 2.0.py
+++ 포인트 2.0.py
@@ -1,10 +1,4 @@
-# 20201003_기술체크
-# data = 전화번호, 금액, 포인트
-# 1회당 금액의 10%씩 포인트 적립
-# 도도포인트 ==> 글로벌 해짐!
-
-import telnetlib
-
+import time
 
 prompt = '''
 ==================
@@ -23,9 +17,10 @@ prompt = '''
 
 # 변수선언/주화면 보여주기
 menu = True
-
+welcome_point = 100
 dtPoint = {"01000000000": 100}
-
+dtPoint_copy = dtPoint.copy()
+last_add_point = 0
 print(prompt)
 
 # menu 선택
@@ -38,27 +33,58 @@ while menu != "0":
         if not len(dtPoint):
             print("# 포인트 데이터가 없습니다.", "\n")
             continue
-        for tel, point in dtPoint.items():
-            print(f"{tel} {point}")
+        for tel, get_point in dtPoint.items():
+            print(f"{tel} 고객님의 보유 포인트 : {get_point}")
 
     elif menu == "2":  # 전화번호 알고리즘 - 010-0000-0000 (-) 기호없이
         tel = input(" 전화번호를 입력하세요 :  ")
-        if not len(dtPoint):
-            print("# 회원등록을 해주세요. ", "\n")
+        if len(tel) >= 10:
+            if dtPoint.get(tel) == None:
+                print("# 비회원으로 확인되어 회원 등록으로 넘어 갑니다 . ", "\n")
+                time.sleep(1)
+                if dtPoint.get(tel) == None:
+                    dtPoint[tel] = welcome_point
+                    print(f"# 회원 등록을 되었습니다. ")
+                    print(f"# 등록 축하 포인트 : {dtPoint[tel]}point \n")
+                    continue
+            else:
+                price = int(input(" 주문 금액을 입력하세요 : "))
+
+                newPoint = int(price*0.01)
+                last_add_point = newPoint
+                dtPoint[tel] = dtPoint.get(
+                    tel) + newPoint  # 수정 (기존 포인트에서 업데이트)
+                # 가장 마지막에 추가된 포인트만 계속 업데이트 !
+                dtPoint_copy.update({tel: newPoint})
+                print(
+                    f"# {newPoint} point가 추가적립되어 현재 {dtPoint[tel]} point 입니다.", "\n")
         else:
-            price = int(input(" 주문 금액을 입력하세요 : "))
-            newPoint = int(price*0.1)
-            dtPoint[tel] = point + newPoint  # 수정 (기존 포인트에서 업데이트)
-            print(
-                f"# {newPoint} point가 추가적립되어 현재 {dtPoint[tel]} point 입니다.", "\n")
+            print("전화번호를 다시 한번 확인해 주세요 ! ")
 
     elif menu == "3":
-        tel = input("  포인트 수정이 필요한 전화번호를 입력하세요 : ")
+        tel = input("  point 수정이 필요한 전화번호를 입력하세요 : ")
 
         if dtPoint.get(tel):
-            point = int(input("  최종포인트를 입력해주세요 : "))
-            dtPoint[tel] = point
-            print(f"# {point} point 로 수정되었습니다.", "\n")
+            print(f"{tel}님이 현재까지 보유중인 point : {dtPoint[tel]} ")
+            # 마지막으로 들어가 포인트 -> 수정하는게 맞는 거잖아
+            if dtPoint_copy.get(tel) == None:
+                print(f" 마지막 추가 point : 0 ")
+            else:
+                print(f" 마지막 추가 point : {dtPoint_copy.get(tel)} ")
+            # 마지막 추가 포인트 : copy -> update
+            change_point = int(input("수정할 포인트를 입력해주세요 : "))  # 0 입력 예외 처리
+            if change_point > 0:
+                dtPoint[tel] = dtPoint.get(
+                    tel) - dtPoint_copy.get(tel) + change_point
+                print(
+                    f"#기존 추가 된 {dtPoint_copy.get(tel)} point 입력하신 {change_point} point 로 수정되었습니다.", "\n")
+                dtPoint_copy.update({tel: change_point})
+            elif change_point == 0:
+                print(f"기존 포인트 {dtPoint.get(tel)} 유지됩니다.")
+                pass
+            else:
+                print("0 이상의 정확한 숫자를 입력하세요")
+
         else:
             print(f"# {tel}님은 등록되어 있지 않습니다. ", "\n")
 
@@ -67,7 +93,7 @@ while menu != "0":
 
         if dtPoint.get(tel):
             print(f"현재 point : {point} point ")
-            usePoint = int(input(" 사용하실 포인트를 적어주세요. :"))
+            usePoint = int(input(" 사용하실 point를 적어주세요. :"))
             dtPoint[tel] = point - usePoint
             print(f"# {usePoint} point를 사용하였습니다. ")
             print(f"# {dtPoint[tel]} point가 남아있습니다. ", "\n")
@@ -97,23 +123,19 @@ while menu != "0":
             print("# 이전 포인트데이터를 가져왔습니다.!", "\n")
 
     elif menu == "7":  # 동일한 전화 번호가 존재 하면 이미 존재하는 번호라고 고지 하기
-        dtPoint_copy = dtPoint.copy()
-        print(dtPoint_copy)
-        tel = input("  전화번호를 입력하세요: ")
-        point = 100
-        if len(tel) < 11 and len(tel) > 10:
-            print(f"정확한 연락처를 확인해주세요 : {tel} 잘못 입력하셨습니다.")
+        tel = input("전화번호를 입력하세요: ")
+
+        if len(tel) >= 10:
+            if dtPoint.get(tel) == None:
+
+                dtPoint[tel] = welcome_point
+                print(f"# 회원 등록을 되었습니다. ")
+                print(f"# 등록 축하 포인트 : {dtPoint[tel]}point \n")
+            else:
+                print(f"이미 회원이시네요!! {tel} 회원님의 포인트 : {dtPoint[tel]}")
+
         else:  # 번호가 인증 되면
-            print(dtPoint_copy)
-            for k, v in dtPoint_copy.items():
-                print(f"check {k} , {v}")
-                if tel in k:
-                    print(f"# 이미 우리 회원이시네요 ! 현재 포인트는 {dtPoint[k]} ")
-                    break
-                else:
-                    dtPoint[tel] = point
-                    print(f"# 회원 등록을 되었습니다. ")
-                    print(f"# 등록 축하 포인트 : {dtPoint[tel]}point \n")
+            print(f"정확한 연락처를 확인해주세요 : {tel} 잘못 입력하셨습니다.")
 
     else:
         print("잘못 입력했습니다. 다시 입력해주세요. \n")
